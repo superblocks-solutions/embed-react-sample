cc81149a4fd4e8c6f40206ddaee637c0977d9001
Kris Harbold <harbolkn@gmail.com>
2024-03-06 17:16:25 -0500

    Add cors support
    Add cors support

--- Sample-01/api-server.js
+++ Sample-01/api-server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const app = express();
 
 const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = `http://localhost:${appPort}`;
+
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors({ origin: appOrigin }));
 
 // Get a user token from Superblocks.
 app.get("/api/superblocks/token", (req, res) => {
